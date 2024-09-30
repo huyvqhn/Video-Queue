258bf4db978fb66b63e6d2065cb291d58b92d53f
HVOQUAN <Huy.VoQuang@nttdata.com>
2024-09-30 16:53:03 +0700

    feature/VPMQ-0005-separte-receive-processed
    + Separate 2 directories: recieve and processed
    +

--- src/main/java/com/videoqueue/videoqueueprocessing/controller/VideoController.java
+++ src/main/java/com/videoqueue/videoqueueprocessing/controller/VideoController.java
@@ -1,11 +1,14 @@
 package com.videoqueue.videoqueueprocessing.controller;
 
+import com.videoqueue.videoqueueprocessing.model.Resolution;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,8 +18,11 @@ import java.io.File;
 @RequestMapping("videos")
 public class VideoController {
 
-    @Value("${upload.directory}")
-    private String uploadDirectory;
+    @Value("${processed.directory}")
+    private String processedDirectory;
+
+    @Value("${receive.directory}")
+    private String receiveDirectory;
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
@@ -29,33 +35,39 @@ public class VideoController {
 
     private static final Logger logger = LoggerFactory.getLogger(VideoController.class);
 
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam String filePath) {
-        filePath = "D:\\017-LAP-TRINH\\06-MikaylVideoProcess\\dummy-video.mp4";
-        String resolution = "480";
-//        processVideoWithResolution("uploads", filePath, resolution);
-
-        return "Hi";
-    }
-
     @PostMapping("/upload")
-    public String uploadVideo(@RequestParam("file") MultipartFile file)  {
+    public ResponseEntity<String> uploadVideo(@RequestParam("file") MultipartFile file,
+                                              @RequestParam("resolution") String resolution)
+        throws Exception {
 
-        String filePath = uploadDirectory + "/" + file.getOriginalFilename();
+        String filePath = receiveDirectory + file.getOriginalFilename();
+        if (StringUtils.isEmpty(resolution)) {
+            throw new Exception("The input resolution is empty");
+        }
+        if (!resolution.equals(Resolution.R480.name()) && !resolution.equals(Resolution.R720.name())) {
+            throw new IllegalArgumentException("The input resolution is not supported");
+        }
+
+
         try {
+            new File(processedDirectory).mkdirs();
+
             // Save the uploaded file to the specified directory
             File dest = new File(filePath);
-
             dest.getParentFile().mkdirs(); // Create directories if they don't exist
             file.transferTo(dest);
 
-            // Send tasks to RabbitMQ queues
-            rabbitTemplate.convertAndSend(video480pQueue.getName(), filePath);
-            rabbitTemplate.convertAndSend(video720pQueue.getName(), filePath);
+            // Send tasks to RabbitMQ queues based on resolution
+            if (Resolution.R480.name().equals(resolution)) {
+                rabbitTemplate.convertAndSend(video480pQueue.getName(), filePath);
+            } else if (Resolution.R720.name().equals(resolution)) {
+                rabbitTemplate.convertAndSend(video720pQueue.getName(), filePath);
+            }
         } catch (Exception e) {
-            System.out.printf("e");
+            logger.error("Error uploading video: ", e);
+            ResponseEntity.ok("Error uploading video");
         }
 
-        return "Video uploaded successfully";
+        return ResponseEntity.ok("Video uploaded successfully");
     }
 }
--- src/main/java/com/videoqueue/videoqueueprocessing/service/VideoProcessingService.java
+++ src/main/java/com/videoqueue/videoqueueprocessing/service/VideoProcessingService.java
@@ -1,9 +1,11 @@
 package com.videoqueue.videoqueueprocessing.service;
 
+import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
@@ -14,28 +16,37 @@ public class VideoProcessingService {
 
     private static final Logger logger = LoggerFactory.getLogger(VideoProcessingService.class);
     public static final String UPLOADS = "uploads";
+    public static final String FFMPEG_EXE = "D:\\ffmpeg\\bin\\ffmpeg.exe";
+    public static final String SCALE_480 = "scale=-640:480";
+    public static final String SCALE_720 = "scale=-1280:720";
 
     @Autowired
     private QueueLockService queueLockService;
+
+    @Value("${processed.directory}")
+    private String processedDirectory;
 
     @RabbitListener(queues = "video_480p")
     public void process480pVideo(String filePath) {
         if (queueLockService.tryLock()) {
             try {
-                processVideoWithResolution(UPLOADS, filePath, "480p");
+                processVideoWithResolution(UPLOADS, filePath, this.fileNameFromPath(filePath), "480p");
             } finally {
                 logger.info("upload 480p file success");
                 queueLockService.unlock();
             }
         }
+    }
 
+    private String fileNameFromPath(String filePath) {
+        return filePath.substring(filePath.lastIndexOf("\\") + 1);
     }
 
     @RabbitListener(queues = "video_720p")
     public void process720pVideo(String filePath) {
         if (queueLockService.tryLock()) {
             try {
-                processVideoWithResolution(UPLOADS, filePath, "720p");
+                processVideoWithResolution(UPLOADS, filePath, this.fileNameFromPath(filePath), "720p");
             } finally {
                 logger.info("upload 720p file success");
                 queueLockService.unlock();
@@ -43,15 +54,15 @@ public class VideoProcessingService {
         }
     }
 
-    private void processVideoWithResolution(String action, String filePath, String resolution) {
+    private void processVideoWithResolution(String action, String filePath, String fileName, String resolution) {
 
-        String outputFilePath = filePath.replace(action, "processed").replace(".mp4", "_" + resolution + ".mp4");
+        String outputFilePath = processedDirectory + fileName.replace(action, "processed").replace(".mp4", "_" + resolution + ".mp4");
         String[] command = {
-                "ffmpeg",
+                FFMPEG_EXE,
                 "-i",
                 filePath,
                 "-vf",
-                "scale=-1:480",
+            Objects.equals(resolution, "480p") ? SCALE_480 : SCALE_720,
                 outputFilePath
         };
         try {
@@ -60,26 +71,25 @@ public class VideoProcessingService {
 
             // Redirect error stream to the standard output stream
             processBuilder.redirectErrorStream(true);
+            logger.info("Command call ffmpeg: " + processBuilder.command().toString());
 
             // Start the process
             Process process = processBuilder.start();
 
             // Read the output
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
             }
 
             // Wait for the process to complete
-            int exitCode = process.waitFor();
-            reader.close();
+//            int exitCode = process.waitFor();
 
-            System.out.println("Exited with code: " + exitCode);
         } catch (Exception e) {
             logger.error("Error while processing video:  " + e.getCause().toString());
+            System.out.println("Error while processing video:  " + e.getCause().toString());
         }
     }
 }
