3f1d20788ed7f5e07f808cbb66926c528e303e46
HVOQUAN <Huy.VoQuang@nttdata.com>
2024-10-01 15:37:45 +0700

    feature/VPMQ-0005-separte-receive-processed

--- src/main/java/com/videoqueue/videoqueueprocessing/config/RabbitMQConfig.java
+++ src/main/java/com/videoqueue/videoqueueprocessing/config/RabbitMQConfig.java
@@ -1,11 +1,19 @@
 package com.videoqueue.videoqueueprocessing.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        return new FanoutExchange("video_queue_exchange");
+    }
 
     @Bean
     public Queue video480pQueue() {
@@ -15,5 +23,15 @@ public class RabbitMQConfig {
     @Bean
     public Queue video720pQueue() {
         return new Queue("video_720p", true);
+    }
+
+    @Bean
+    public Binding binding480p(FanoutExchange fanoutExchange, Queue video480pQueue) {
+        return BindingBuilder.bind(video480pQueue).to(fanoutExchange);
+    }
+
+    @Bean
+    public Binding binding720p(FanoutExchange fanoutExchange, Queue video720pQueue) {
+        return BindingBuilder.bind(video720pQueue).to(fanoutExchange);
     }
 }
--- src/main/java/com/videoqueue/videoqueueprocessing/service/FFmpegProcessManager.java
+++ src/main/java/com/videoqueue/videoqueueprocessing/service/FFmpegProcessManager.java
@@ -1,21 +1,44 @@
 package com.videoqueue.videoqueueprocessing.service;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Huy.VoQuang
  */
-public class FFmpegProcessManager {
+@Component
+@Getter
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+public class FFmpegProcessManager extends Process {
+
+    private static final Logger logger = LoggerFactory.getLogger(VideoProcessingService.class);
+
     private Process ffmpegProcess;
+
+    private String resolution;
 
     public void startFFmpeg(String[] command) throws IOException {
         ProcessBuilder processBuilder = new ProcessBuilder(command);
+        // Redirect error stream to the standard output stream
+        processBuilder.redirectErrorStream(true);
+        //        logger.info("Command call ffmpeg: " + processBuilder.command().toString());
+
+        String resolution = command[4];
+        this.resolution = resolution.substring(resolution.indexOf(":") + 1);
         ffmpegProcess = processBuilder.start();
     }
 
     public void pauseFFmpeg() throws IOException {
         if (ffmpegProcess != null) {
             String pid = Long.toString(ffmpegProcess.pid());
+            logger.info(pid + " is paused");
             new ProcessBuilder("cmd", "/c", "pause", pid).start();
         }
     }
@@ -23,7 +46,46 @@ public class FFmpegProcessManager {
     public void resumeFFmpeg() throws IOException {
         if (ffmpegProcess != null) {
             String pid = Long.toString(ffmpegProcess.pid());
+            logger.info(pid + " is resumed");
             new ProcessBuilder("cmd", "/c", "resume", pid).start();
         }
+    }
+
+    public boolean isRunning() {
+        return ffmpegProcess != null && ffmpegProcess.isAlive();
+    }
+
+    public boolean isPaused() {
+        // Implement logic to check if the process is paused
+        return false;
+    }
+
+    @Override
+    public OutputStream getOutputStream() {
+        return null;
+    }
+
+    public InputStream getInputStream() {
+        return this.ffmpegProcess.getInputStream();
+    }
+
+    @Override
+    public InputStream getErrorStream() {
+        return null;
+    }
+
+    @Override
+    public int waitFor() throws InterruptedException {
+        return 0;
+    }
+
+    @Override
+    public int exitValue() {
+        return 0;
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }
